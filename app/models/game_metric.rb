c6480b643217819c5774b5e727248293be04641d
Mahendra Choudhary <mahendrachoudhary1156@gmail.com>
2021-06-27 20:05:12 +0530

    added assocations

--- app/models/game_metric.rb
+++ app/models/game_metric.rb
@@ -1,2 +1,4 @@
 class GameMetric < ApplicationRecord
+  has_one :user, foreign_key: :id
+  belongs_to :game
 end
--- app/models/leader_board.rb
+++ app/models/leader_board.rb
@@ -1,2 +1,3 @@
 class LeaderBoard < ApplicationRecord
+  belongs_to :user, foreign_key: :id
 end
