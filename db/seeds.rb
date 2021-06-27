024972a042e5bc9bd044583704a7a7a623a05c7a
Mahendra Choudhary <mahendrachoudhary1156@gmail.com>
2021-06-27 19:13:05 +0530

    add sample seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "creating user"
+player_one = User.create(name: 'john doe', phone_number: '9988776655')
+player_two = User.create(name: 'cynthia smith', phone_number: '9988778885')
+
+puts "creating game"
+game = Game.create(player_one_id: player_one.id, player_two_id: player_two.id)
+
+puts "creating game metrics"
+GameMetric.create(player_id: player_one.id, result: 'win', game_id: game.id)
+GameMetric.create(player_id: player_two.id, result: 'loss', game_id: game.id)
+
+puts "creating leader board data"
+LeaderBoard.create(player_id: player_one.id,  score: 10, total_games: 1,  win: 1)
+LeaderBoard.create(player_id: player_two.id,  score: 0, total_games: 1,  loss: 1)
