623d8df24ba3ab69be8cc850ff66bc63e9a87faf
Mahendra Choudhary <mahendrachoudhary1156@gmail.com>
2021-06-28 17:50:32 +0530

    minor fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User <  ApplicationRecord
   validates :phone_number, presence: true, length: {is: 10}
   
   # Association
-  has_many :game_metrics, foreign_key: :player_id
+  has_many :game_metrics
   has_one :leader_board
 end
