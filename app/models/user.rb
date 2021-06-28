bc73e0d7fb6f78ddad535f6782356a85cefdc171
Mahendra Choudhary <mahendrachoudhary1156@gmail.com>
2021-06-28 13:51:54 +0530

    added validations for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User <  ApplicationRecord
+  # Validations
+  validates :name, presence: true
+  validates :phone_number, presence: true, length: {is: 10}
+  
+  # Association
   has_many :game_metrics, foreign_key: :player_id
 end
