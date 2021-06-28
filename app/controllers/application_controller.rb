d60c3c14f63bfc7759af9fa571c7ed84a353f823
Mahendra Choudhary <mahendrachoudhary1156@gmail.com>
2021-06-28 13:52:33 +0530

    global method for app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+
+  def render_404
+    redirect_to new_registration_path, notice: 'Register before playing scrable'
+  end
+
+  def current_user
+    @_current_user ||= session[:current_user_id] && User.find(session[:current_user_id])
+  end
+  helper_method :current_user
 end
